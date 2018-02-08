7da93c4d7f51c84c61f2c35630e88fa48ca5f5ba
José Manuel Nieto Sánchez <superjmn@outlook.com>
2018-02-08 15:18:45 +0100

    Updated to latest versions and removed debug code. Still failing when saving to XML (Serialization with EXS)

--- Designer.Plugin.Default/DefaultImporter.cs
+++ Designer.Plugin.Default/DefaultImporter.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Designer.Model;
 using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel;
 using ExtendedXmlSerializer.ExtensionModel.Content;
 using ExtendedXmlSerializer.ExtensionModel.Xml;
 
@@ -19,6 +20,7 @@ namespace Designer.Plugin.Default
                 .UseAutoFormatting()
                 .UseOptimizedNamespaces()                
                 .EnableParameterizedContent()
+                .Type<TextBox>().Member(x => x.Text).Verbatim()
                 .Register(ColorConverter.Default)
                 .Create();
         }
@@ -31,21 +33,7 @@ namespace Designer.Plugin.Default
         }
 
         public override Task Save(Stream stream, IList<Document> documents)
-        {
-            var textBox = new TextBox()
-            {
-                Text =
-                    "{\\rtf1\\fbidis\\ansi\\ansicpg1252\\deff0\\nouicompat\\deflang3082{\\fonttbl{\\f0\\fnil Segoe UI;}}\r\n{\\colortbl ;\\red0\\green0\\blue0;}\r\n{\\*\\generator Riched20 10.0.16299}\\viewkind4\\uc1 \r\n\\pard\\tx720\\cf1\\f0\\fs23 Sample Text\\par\r\n}"
-            };
-            var docs = new List<Document>()
-            {
-                new Document() 
-                {
-                    Graphics = { textBox }
-                }
-            };
-
-
+        {           
             serializer.Serialize(stream, documents);
             return Task.CompletedTask;
         }
